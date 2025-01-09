e23cf6e6afa1163037ff024b4272a2a9571b68ca
Karthik Bashyam <karthikb4ps@gmail.com>
2025-01-08 21:14:45 -0500

    Made changes to config file

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    host: '0.0.0.0', // Allow access from external devices
+    port: 8080,      // Specify the port
+    allowedHosts: 'all', // Allow all hosts
+    // allowedHosts: ['your-custom-domain.com'], // Allow specific domains
+  }
+};
